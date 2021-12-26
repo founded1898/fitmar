0f7234a994e3f01b556d46c66becc92820d28670
Jaro Schwab <jaro@zeilenwerk.ch>
2021-12-26 12:34:03 +0100

    image + circles

--- web/components/Drawer.tsx
+++ web/components/Drawer.tsx
@@ -1,11 +1,23 @@
-import React from "react";
-import { Stage, Layer, Line, Text } from "react-konva";
-import dynamic from "next/dynamic";
+import React, { useEffect, useRef } from "react";
+import { Image, Stage, Layer, Line, Text, Circle } from "react-konva";
+import { KonvaEventObject } from "konva/lib/Node";
+import useImage from "use-image";
+
+const IMAGE_SRC = "/images/bg.jpg";
 
 export const Drawer = () => {
   const [tool, setTool] = React.useState("pen");
   const [lines, setLines] = React.useState([]);
+  const [circles, setCircles] = React.useState([]);
+
   const isDrawing = React.useRef(false);
+  const [image, state] = useImage(IMAGE_SRC);
+  const stageRef = useRef(null);
+
+  useEffect(() => {
+    if (state === "loaded") {
+    }
+  }, [state, image]);
 
   const handleMouseDown = (e) => {
     isDrawing.current = true;
@@ -13,7 +25,7 @@ export const Drawer = () => {
     setLines([...lines, { tool, points: [pos.x, pos.y] }]);
   };
 
-  const handleMouseMove = (e) => {
+  const handleMouseMove = (e: KonvaEventObject<TouchEvent>) => {
     // no drawing - skipping
     if (!isDrawing.current) {
       return;
@@ -29,24 +41,49 @@ export const Drawer = () => {
     setLines(lines.concat());
   };
 
+  const handleClick = (e: KonvaEventObject<MouseEvent>) => {
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+    console.log("point", point);
+
+    setCircles((prev) => [...prev, [point.x, point.y]]);
+  };
+
   const handleMouseUp = () => {
     isDrawing.current = false;
   };
 
+  if (!image) {
+    return null;
+  }
+
+  const handleExport = () => {
+    if (stageRef.current) {
+      const uri = stageRef.current.toDataURL();
+      console.log(uri);
+    }
+  };
+
   return (
     <div>
+      <button name="asmdkasd" onClick={handleExport}>
+        asdmkasd
+      </button>
       <Stage
-        width={window.innerWidth}
-        height={window.innerHeight}
+        width={image.width}
+        height={image.height}
         onMouseDown={handleMouseDown}
         onMousemove={handleMouseMove}
         onMouseup={handleMouseUp}
         onTouchStart={handleMouseDown}
         onTouchEnd={handleMouseUp}
         onTouchMove={handleMouseMove}
+        onClick={handleClick}
+        ref={stageRef}
       >
         <Layer>
           <Text text="Just start drawing" x={5} y={30} />
+          <Image image={image} fillPatternImage={image} />
           {lines.map((line, i) => (
             <Line
               key={i}
@@ -59,6 +96,9 @@ export const Drawer = () => {
                 line.tool === "eraser" ? "destination-out" : "source-over"
               }
             />
+          ))}
+          {circles.map(([x, y]) => (
+            <Circle x={x} y={y} radius={10} fill="green" />
           ))}
         </Layer>
       </Stage>
